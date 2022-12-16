8a9674264549975710a9b99d7dde2c82f65ab5e5
George <gio.digmel@gmail.com>
2022-12-16 15:00:48 -0600

    fix: added card images

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,15 +1,29 @@
 import React from "react";
+import Image from "next/image";
 
 type TCard = {
   title: string;
   subtitle: string;
+  image?: string;
   backgroundStyle?: string;
 };
 
-export const Card = ({ title, subtitle, backgroundStyle }: TCard) => {
+export const Card = ({ title, subtitle, image, backgroundStyle }: TCard) => {
   return (
     <div className="md:w-1/2 flex flex-col mx-6 mb-12">
-      <div className={`md:h-96 h-64 mb-4 rounded ${backgroundStyle}`}></div>
+      <div
+        className={`md:h-96 h-64 mb-4 rounded flex justify-center items-center ${backgroundStyle}`}
+      >
+        {image && (
+          <Image
+            src={`/assets/${image}.png`}
+            width={390}
+            height={507}
+            alt="1"
+            quality={30}
+          />
+        )}
+      </div>
       <h1 className="text-sm font-bold text-primary-dark mb-2">{title}</h1>
       <p className="text-sm font-regular text-body">{subtitle}</p>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,21 @@
 import { Card, Layout } from "../components";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout title="eleneux.com">
       <div className="flex flex-col">
-        {/* <Image
-          src="/assets/image1.png"
-          width={390}
-          height={507}
-          alt="1"
-          quality={30}
-        /> */}
-
         <div className="md:ml-24 md:-mx-6 flex flex-grow flex-col md:flex-row">
           <Card
             title="Learn First Aid"
             subtitle="App allows people to refresh their First Aid knowledge with illustrated articles and quizzes."
+            image="first-aid-hero"
             backgroundStyle="bg-gradient-to-r from-blue-100 to-blue-200"
           />
 
           <Card
             title="Gluten Free Finder"
             subtitle="App guides people to discover Gluten Free restaurants worldwide."
+            image="gluten-free-hero"
             backgroundStyle="bg-gradient-to-r from-green-100 to-green-200"
           />
         </div>
@@ -31,12 +24,14 @@ export default function Home() {
           <Card
             title="Reduce Food Waste"
             subtitle="App allows people to share Food & Grocery products with NGO communities."
+            image="food-waste-hero"
             backgroundStyle="bg-gradient-to-r from-orange-100 to-orange-200"
           />
 
           <Card
             title="Fashion Designer's Website"
             subtitle="Responsive portfolio website, where people can find designer’s collection and contact details."
+            image="marikone-hero"
             backgroundStyle="bg-gradient-to-r from-purple-100 to-purple-200"
           />
         </div>
