19abf52b2208f25773e56c98230736f15d06da84
George <gio.digmel@gmail.com>
2022-12-26 17:36:27 -0600

    chore: added menu colors

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect, ReactNode } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { Text } from "./Text";
 
 type TMenuItemVariant = "web" | "mobile";
@@ -17,18 +18,35 @@ export const MenuItem = ({
   isActive = false,
   link = "/",
 }: TMenuItem) => {
+  const { pathname } = useRouter();
+  const [color, setColor] = useState("");
+
+  useEffect(() => {
+    switch (pathname) {
+      case "/learn-first-aid-app":
+        setColor("text-blue-400");
+        break;
+      case "/gluten-free-finder-app":
+        setColor("text-green-400");
+        break;
+    }
+  }, []);
+
   return (
     <Link className="flex flex-row text-end justify-end my-2" href={link}>
       {variant && (
         <Text
           variant={`${isActive ? "menu-active" : "menu-default"}`}
-          className={`mr-1 ${isActive ? "text-active" : "text-gray-200"}`}
+          className={`mr-1 ${isActive ? color : "text-gray-200"}`}
         >
           {variant === "web" ? "Web |" : "Mobile |"}
         </Text>
       )}
 
-      <Text variant={`${isActive ? "menu-active" : "menu-default"}`}>
+      <Text
+        variant={`${isActive ? "menu-active" : "menu-default"}`}
+        className={`${isActive && color}`}
+      >
         {title}
       </Text>
     </Link>
