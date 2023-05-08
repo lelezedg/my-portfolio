1af4555db7192ced8e87b4e1e1f188e83e1a1755
George <gio.digmel@gmail.com>
2023-05-08 03:25:15 -0500

    fix: clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
-import { Head } from "next/document";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-87ETS7Y1TK"
         strategy="afterInteractive"
