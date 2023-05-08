87d1f2c8bc4d0e37a1d2a13cf94f7a833148f76b
George <gio.digmel@gmail.com>
2023-05-08 03:21:08 -0500

    chore: adding google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import { Head } from "next/document";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-87ETS7Y1TK"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-87ETS7Y1TK');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
