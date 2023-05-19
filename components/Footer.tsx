42a7aa1199c83a8591f8e09f6c09e2ab4528afa1
George <gio.digmel@gmail.com>
2023-05-20 00:07:25 +0400

    fix: copyrights

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
   return (
     <div className="py-4 border-t border-gray-100 border-opacity-50 flex justify-center md:mt-8 mt-24  ">
       <Text variant="body" className="text-xs md:text-sm">
-        2022 All rights reserved © Elene Zedginidze
+        © 2021-23 Elene Zedginidze. All rights reserved.
       </Text>
     </div>
   );
